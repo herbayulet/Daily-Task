57fc9331bdc4ef7e52b5a50738f7f0a30d3510d2
herbayulet <sayaherbayu@gmail.com>
2022-06-01 11:41:11 +0700

    update additional task

---
+++ arrayMethod.js
@@ -0,0 +1,58 @@
+let dataHandling2 = (x) => {
+  // Method Splice
+  x.splice(1, 2, "Roman Alamsyah Elsharawy", "Provinsi Bandar Lampung"), x.splice(3, 1, "21/05/1989"), x.splice(4, 1, "Pria", "SMA International Metro");
+  console.log(x);
+
+  //   Tanggal Bulan Tahun
+  let tanggal = x[3].split("/");
+  let bulan = parseInt(tanggal[1]);
+  switch (bulan) {
+    case 01:
+      console.log("Januari");
+      break;
+    case 02:
+      console.log("Februari");
+      break;
+    case 03:
+      console.log("Maret");
+      break;
+    case 04:
+      console.log("April");
+      break;
+    case 05:
+      console.log("Mei");
+      break;
+    case 06:
+      console.log("Juni");
+      break;
+    case 07:
+      console.log("Juli");
+      break;
+    case 08:
+      console.log("Agustus");
+      break;
+    case 09:
+      console.log("September");
+      break;
+    case 10:
+      console.log("Oktober");
+      break;
+    case 11:
+      console.log("November");
+      break;
+    case 12:
+      console.log("Desember");
+      break;
+    default:
+      console.log("Gak ada bulan ke 13");
+      break;
+  }
+  let handlingBulan = tanggal.join("-");
+
+  let urut = tanggal.sort((a, b) => b - a);
+  console.log(urut);
+  console.log(handlingBulan);
+  let nama = x[1].slice(0, 15);
+  console.log(nama);
+};
+dataHandling2(["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1998", "Membaca"]);
--- readme.md
+++ readme.md
@@ -1,7 +1,9 @@
 Pengerjaan Daily Task :
 Ini adalah Study Case yang diberikan terkait :
+
 1. Variable
 2. Conditional
 3. Looping
 4. Array
 5. Function
+   \*\* untuk additional task ini melatih kita untuk bisa memahami fungsi-fungsi atau beberapa metode yang ada dalam bahasa pemograman
